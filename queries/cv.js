cb837d470b443cfdc3c84b0e3ea82b989a0922bf
joffrey.marc <joffrey.marc@epita.fr>
2019-10-12 23:34:12 +0200

    REMOVE CAPITAL LETTERS !!!!!!!

--- queries/cv.js
+++ queries/cv.js
@@ -49,24 +49,27 @@ function addCV(req, res, next) {
 
 function getCV(req, res, next) {
     var id_user = req.params.id_user;
-    db.query("SELECT * FROM CV WHERE id_user = ?", [id_user], function (error, results, fields) {
+    db.query("SELECT * FROM cv WHERE id_user = ?", [id_user], function (error, results, fields) {
         if (error) {
+            console.log("error 1 = ", error)
             res.status(500)
                 .json({
                     status: "ko"
                 })
             return;
         }
-        db.query("SELECT * FROM FORMATIONS WHERE FORMATIONS.id_cv = ?", [results[0].id], function (error2, formations, fields2) {
+        db.query("SELECT * FROM formations WHERE formations.id_cv = ?", [results[0].id], function (error2, formations, fields2) {
             if (error2) {
+                console.log("error 2 = ", error2)
                 res.status(500)
                     .json({
                         status: "ko"
                     })
                 return;
             }
-            db.query("SELECT * FROM EXPERIENCE WHERE EXPERIENCE.id_cv = ?", [results[0].id], function (error3, experiences, fields2) {
+            db.query("SELECT * FROM experience WHERE experience.id_cv = ?", [results[0].id], function (error3, experiences, fields2) {
                 if (error3) {
+                    console.log("error 3 = ", error3)
                     res.status(500)
                         .json({
                             status: "ko"
@@ -91,7 +94,7 @@ function modifyCV(req, res, next) {
     auth.verifyJWTToken(token)
         .then((decodedToken) => {
             var id_user = decodedToken.data.id
-            db.query("UPDATE CV SET description = ? WHERE id_user = ?", [description, id_user], function (error, results, fields) {
+            db.query("UPDATE cv SET description = ? WHERE id_user = ?", [description, id_user], function (error, results, fields) {
                 if (error) {
                     res.status(500)
                         .json({
