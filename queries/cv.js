11ee2595e5644144772bcd12f70cfa6b84171a08
Baptiste Saint-André <baptiste@mkdmpartner.com>
2019-10-12 17:04:48 +0200

    Update cv description

--- queries/cv.js
+++ queries/cv.js
@@ -49,10 +49,8 @@ function addCV(req, res, next) {
 
 function getCV(req, res, next) {
     var id_user = req.params.id_user;
-    console.log(id_user)
     db.query("SELECT * FROM CV WHERE id_user = ?", [id_user], function (error, results, fields) {
         if (error) {
-            console.log(error)
             res.status(500)
                 .json({
                     status: "ko"
@@ -61,15 +59,12 @@ function getCV(req, res, next) {
         }
         db.query("SELECT * FROM FORMATIONS WHERE FORMATIONS.id_cv = ?", [results[0].id], function (error2, formations, fields2) {
             if (error2) {
-                console.log(error2)
                 res.status(500)
                     .json({
                         status: "ko"
                     })
                 return;
             }
-
-            console.log(formations)
             res.status(200)
                 .json({
                     status: "ok",
@@ -81,5 +76,32 @@ function getCV(req, res, next) {
 }
 
 function modifyCV(req, res, next) {
-    
+    var token = req.headers.authorization;
+    var description = req.body.description;
+    auth.verifyJWTToken(token)
+        .then((decodedToken) => {
+            var id_user = decodedToken.data.id
+            db.query("UPDATE CV SET description = ? WHERE id_user = ?", [description, id_user], function (error, results, fields) {
+                if (error) {
+                    res.status(500)
+                        .json({
+                            status: "ko"
+                        })
+                    return;
+                }
+                res.status(200)
+                    .json({
+                        status: "ok",
+                        data: "CV updated"
+                    })
+            })
+        })
+        .catch((err) => {
+            res.status(401)
+                .json({
+                    status: "ko",
+                    message: err
+                })
+            return;
+        })
 }
