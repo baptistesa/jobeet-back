c135d972c4a18a218f2c818d2a16cb5da0e3ccd4
Bertrand Ha <bertrand.ha@epita.fr>
2019-10-12 19:01:51 +0200

    [EXPERIENCE] Experience remove

--- routes/experience.js
+++ routes/experience.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var experience = require("../queries/experience");
 
 router.post('/add', experience.addExperience);
+router.post('/delete', experience.removeExperience);
 
 module.exports = router;
