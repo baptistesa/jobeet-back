de79093ef37124c0e3527ed303bc2a2d863a619b
joffrey.marc <joffrey.marc@epita.fr>
2019-10-16 00:05:44 +0200

    Send Sms activate

--- queries/users.js
+++ queries/users.js
@@ -76,7 +76,7 @@ function signup(req, res, next) {
                             status: "ko"
                         })
                 else {
-                    // sendSMS(code, name, phone)
+                    sendSMS(code, name, phone)
                     db.query("SELECT * FROM users WHERE mail=? AND phone=?", [mail, phone], function (error, results, fields) {
                         var token = jwt.createJWToken(results[0]);
                         db.query("INSERT INTO cv(id_user) VALUES (?)", [results[0].id], function (error, results2, fields) {
